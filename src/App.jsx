cd5226f2acaf617c04036f4a9353d8d57f76d6be
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2026-01-07 23:15:18 +0600

    header and assets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./Header";
+
 function App() {
   return (
     <>
-      <div className="bg-red-500 text-white text-center p-4 rounded-lg">
-        <h1 className="text-xl font-bold">Welcome to CineRental</h1>
-      </div>
+      <Header />
     </>
   );
 }
