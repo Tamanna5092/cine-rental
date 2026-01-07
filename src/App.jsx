6ed533ae2ad5a78831146b817c5d3a656dfb9528
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2026-01-07 20:05:49 +0600

    basic setup for cine-rental

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 text-white text-center p-4 rounded-lg">
+        <h1 className="text-xl font-bold">Welcome to CineRental</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
