d8ad62e1b27b1079f560ee044ae6fbcda2ac5a3b
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2026-01-07 23:48:52 +0600

    sidebar and movieList create

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import MovieList from "./cinema/MovieList";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <div class="container grid lg:grid-cols-[218px_1fr] gap-14">
+          <Sidebar></Sidebar>
+          <MovieList></MovieList>
+        </div>
+      </main>
     </>
   );
 }
